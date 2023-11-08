e55ac244483194e3c74bfeab8a1aab5d1f3dae31
nusgnojkrap <ahrl1994@gmail.com>
2023-11-08 17:41:00 +0900

    update main screen

---
+++ js/test1.js
@@ -0,0 +1,9 @@
+function displayTime(){
+    var d = new Date();
+    console.log(`time : ${d.toLocaleString()}`)
+}
+
+function displayCity(city){
+    // 해당 city 로 db 조회 후 사진리스트 return
+    return;
+}
